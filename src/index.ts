69b56ce8eaa29638091e988910b9f37553af029e
Mogakamo <mogaka.amo254@gmail.com>
2022-09-04 16:15:52 +0300

    refactor: updating the server for production

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,16 @@ import { ApolloServer } from "apollo-server";
 
 import { schema } from "./schema";
 import { context } from "./context";
+import {ApolloServerPluginLandingPageLocalDefault} from "apollo-server-core"
 
 export const server = new ApolloServer({
   schema,
   context,
+  introspection: true,
+  plugins: [ApolloServerPluginLandingPageLocalDefault()]
 });
 
-const port = 3030;
+const port = process.env.PORT || 3030;
 
 server.listen({ port }).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
