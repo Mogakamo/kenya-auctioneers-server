3bed3303d047f833c7becc1c45ca73901e21bd96
Mogakamo <mogaka.amo254@gmail.com>
2022-09-06 11:38:36 +0300

    chore: setting up the swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Kenyan Auctioneers API')
+    .setDescription(
+      'This is the official API for the Kenyan Auctioneers web application',
+    )
+    .setVersion('1.0')
+    .addTag('Auctioneers')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
