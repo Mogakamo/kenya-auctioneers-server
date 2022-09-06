f58f8259000f15b712f015516fcc19e3f4e450ad
Mogakamo <mogaka.amo254@gmail.com>
2022-09-06 14:42:34 +0300

    chore: working on the users data

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,8 +2,22 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
+export type User = any;
+
 @Injectable()
 export class UsersService {
+  private readonly users = [
+    {
+      userId: 1,
+      username: "mogaka.dev",
+      password: "mogaka"
+    },
+    {
+      userId: 2,
+      username: "john",
+      password: "changeme"
+    }
+  ]
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
@@ -12,8 +26,8 @@ export class UsersService {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find(user => user.username === username)
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
