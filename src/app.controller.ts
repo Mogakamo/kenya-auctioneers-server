58be0686ba8d751c4f5f31fb1d853be170877a2a
Mogakamo <mogaka.amo254@gmail.com>
2022-09-06 20:03:59 +0300

    packages: added the express types

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,13 +3,22 @@ import { Controller, Request, Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthGuard } from '@nestjs/passport';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(private authService: AuthService) {}
+
+
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req: any) {
     return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req: any) {
+    return req.user
   }
 }
