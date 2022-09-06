94a716be7eba886339a952ab994440d3102a8bbd
Mogakamo <mogaka.amo254@gmail.com>
2022-09-06 19:10:08 +0300

    package install: jwt packages

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,20 @@
 import { UsersService } from './../users/users.service';
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private usersService: UsersService, ) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
-    async validateUser(username: string, pass: string): Promise<any> {
-        const user = await this.usersService.findOne(username)
-        if(user && user.password === pass) {
-            const {password, ...result} = user
-            return result
-        }
-        return null
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = await this.usersService.findOne(username);
+    if (user && user.password === pass) {
+      const { password, ...result } = user;
+      return result;
     }
+    return null;
+  }
 }
