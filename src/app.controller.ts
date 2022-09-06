eaafe3186da84786e9b252cac4949e8cd6a47aa5
Mogakamo <mogaka.amo254@gmail.com>
2022-09-06 19:34:21 +0300

    refactor: the option of logging in with a jwt token

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './auth/auth.service';
 import { Controller, Request, Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthGuard } from '@nestjs/passport';
@@ -5,9 +6,10 @@ import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
+  constructor(private authService: AuthService) {}
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req: any) {
-    return req.user;
+    return this.authService.login(req.user);
   }
 }
